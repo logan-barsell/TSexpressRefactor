a9a08e200aed8c2db5736f7fb814179f309651c4
Logan Barsell <loganjbars@gmail.com>
2022-05-14 16:09:49 -0700

    decorators and metadata

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import bodyParser from 'body-parser';
 import express, { Request, Response } from 'express';
-import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+import { AppRouter } from './AppRouter';
+import './controllers/LoginController';
+import './controllers/RootController';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieSession({ keys: ['123'] }));
-app.use(router);
+app.use(cookieSession({ keys: ['laskdjf'] }));
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
-  console.log('listening on port 3000');
+  console.log('Listening on port 3000');
 });
